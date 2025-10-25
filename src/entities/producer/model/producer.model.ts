f3d0719da16c46f2e55ee398c59e33987e2d0ab9
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2025-10-24 23:38:58 -0300

    feat(producers): adiciona modelo de dados para produtores
    Cria a interface Producer e a interface ProducerFormData, que definem a estrutura dos dados dos produtores e os dados necessários para criação e edição. Além disso, adiciona um arquivo de índice para facilitar a exportação dos modelos.

---
+++ src/entities/producer/model/producer.model.ts
@@ -0,0 +1,20 @@
+import { Farm } from '../../farm/model';
+
+/**
+ * Produtor Rural
+ */
+export interface Producer {
+  id: string;
+  cpfCnpj: string;
+  name: string;
+  farms: Farm[];
+}
+
+/**
+ * DTO para criação/edição de produtor
+ */
+export interface ProducerFormData {
+  id?: string;
+  cpfCnpj: string;
+  name: string;
+}
