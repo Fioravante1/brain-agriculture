6cf8b071334d96785932a715bd17b6b6a15061ed
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2025-10-28 16:25:44 -0300

    chore(prisma): atualiza configuração do Next.js para incluir pacotes externos
    Adiciona a configuração de pacotes externos do servidor no next.config.ts, incluindo '@prisma/client' e 'prisma'. Também atualiza a configuração de rastreamento de arquivos de saída para incluir arquivos do cliente Prisma, melhorando a compatibilidade e a organização do projeto.

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
     esmExternals: true,
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
   compiler: {
     reactRemoveProperties: true,
+  },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
   },
 };
 
