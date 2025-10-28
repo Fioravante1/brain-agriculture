5912c9438aa31d7002e3e2e71b7e514661c6115a
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2025-10-28 19:02:39 -0300

    chore(config): adiciona verificação de tipos ao projeto
    Inclui o comando "type-check" no package.json para facilitar a verificação de tipos TypeScript. Atualiza tsconfig.json para incluir tipos do Jest e do Testing Library, melhorando a integração com testes. Ajusta a estrutura de exclusão para simplificar a configuração do TypeScript.

--- src/types/jest-dom.d.ts
+++ src/types/jest-dom.d.ts
@@ -1,1 +1,11 @@
-import '@testing-library/jest-dom';
+/// <reference types="jest" />
+/// <reference types="@testing-library/jest-dom" />
+
+declare namespace jest {
+  interface Matchers<R> {
+    toBeInTheDocument(): R;
+    toHaveAttribute(attribute: string, value?: string): R;
+    toContainElement(element: HTMLElement | null): R;
+    toHaveTextContent(text: string | RegExp): R;
+  }
+}
