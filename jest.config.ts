aac9a6dcc5a94f6852e4d6f374b6731195ec2ce5
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2025-10-28 19:33:54 -0300

    chore(config): atualiza configuração do Jest e TypeScript
    Refatora o arquivo jest.config.ts para incluir novas opções de configuração, como a definição de tipos globais para ts-jest. Atualiza o next-env.d.ts para corrigir o caminho de importação de tipos de rotas. Cria um novo arquivo tsconfig.test.json para configurar o ambiente de testes, garantindo que os tipos do Jest e do Testing Library sejam reconhecidos. Além disso, ajusta o tsconfig.json para melhorar a organização das exclusões e inclusões de arquivos.

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,9 @@ import type { Config } from 'jest';
 import nextJest from 'next/jest.js';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
@@ -15,7 +13,17 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
-  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/**/*.stories.{js,jsx,ts,tsx}', '!src/**/__tests__/**'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/**/__tests__/**',
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.test.json',
+    },
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
